c0d1d42531bb3e538622e5511e986f98e1aeb0d0
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-01-24 20:12:01 +0000

    add validations

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5],
+    message: "Rating must be a value between 0-5" }
+end
